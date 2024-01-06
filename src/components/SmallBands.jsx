72ae42ef0dea2817334a42e9cc8a9e16c4f791e4
JonasBlaedel <119848473+JonasBlaedel@users.noreply.github.com>
2024-01-06 15:48:16 +0100

    small bands

--- src/components/SmallBands.jsx
+++ src/components/SmallBands.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 function SmallBands(props) {
   return (
-    <div className="sm: my-8 grid grid-cols-2 gap-x-2 gap-y-8  sm:grid-cols-3 sm:gap-x-8   md:text-2xl">
+    <div className=" z-40 grid grid-cols-2 gap-x-2 gap-y-8 sm:my-8  sm:grid-cols-3 sm:gap-x-8   md:text-2xl">
       {props.bands.slice(31, 124).map((band) => (
         <Link key={band.slug} bands={band.bands} href={`/bands/${band.slug}`}>
           <h4 className="text-center font-body text-base text-accent hover:underline">
