3bdf4586f631f637772a9234b6986d3cd108b590
Jacob Friis Strand <jacobfriisstrand@gmail.com>
2024-01-06 13:44:10 +0100

    test

--- src/components/TicketBasket.jsx
+++ src/components/TicketBasket.jsx
@@ -24,7 +24,7 @@ function TicketBasket({ children, basketStatus }) {
     <div className="fixed top-10 w-screen rounded bg-opacity-90 p-4 lg:sticky lg:top-10 lg:h-fit lg:w-fit lg:bg-foreground-light lg:shadow-md">
       <animated.aside
         style={slide}
-        className="absolute top-10 z-[1] flex w-[85vw] flex-col justify-between overflow-y-scroll lg:static lg:top-20 lg:block md:w-fit lg:translate-x-0"
+        className="absolute top-10 z-[1] flex w-[85vw] flex-col justify-between overflow-y-scroll md:w-fit lg:static lg:top-20 lg:block lg:translate-x-0"
       >
         <div className="sticky top-4 h-fit w-fit overflow-y-scroll">
           <h4 className="font-heading text-3xl text-text-light">Basket</h4>
