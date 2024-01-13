e61a97fbb9f917978fa35feda5a1ff567d8ae414
Jacob Friis Strand <jacobfriisstrand@gmail.com>
2024-01-13 20:28:24 +0100

    width test

--- src/components/FormGroup.jsx
+++ src/components/FormGroup.jsx
@@ -1,7 +1,9 @@
 function FormGroup({ children, headline, classStyle }) {
   return (
     <fieldset className={`${classStyle} space-y-2`}>
-      <legend className="my-5 w-full font-heading text-2xl text-text-light">{headline}</legend>
+      <legend className="my-5 font-heading text-2xl text-text-light">
+        {headline}
+      </legend>
       {children}
     </fieldset>
   );
